b8fb5a2081879941573acdcee65cc4d0ece5055c
breandan <breandan.considine@jetbrains.com>
2015-12-12 00:09:09 -0500

    Configure project settings
    -Add .idea project settings
    -Customize .gitignore
    -Add required plugins
    -Add default run configurations

--- src/main/kotlin/com/johnlindquist/acejump/AceUtil.kt
+++ src/main/kotlin/com/johnlindquist/acejump/AceUtil.kt
@@ -9,6 +9,7 @@ fun guessBestLocation(editor: Editor): RelativePoint {
     var logicalPosition: VisualPosition = editor.getCaretModel().getVisualPosition()
     return getPointFromVisualPosition(editor, logicalPosition)
 }
+
 fun getPointFromVisualPosition(editor: Editor, logicalPosition: VisualPosition): RelativePoint {
     var p: Point = editor.visualPositionToXY(VisualPosition(logicalPosition.line, logicalPosition.column))
     return RelativePoint(editor.getContentComponent(), p)
