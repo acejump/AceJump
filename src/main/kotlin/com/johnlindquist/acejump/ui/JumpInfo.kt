cfb6896c243481fa191d828804f2d290bd1b8bf2
Breandan Considine <breandan.considine@gmail.com>
2016-10-28 14:24:59 -0400

    Fixes #82

--- src/main/kotlin/com/johnlindquist/acejump/ui/JumpInfo.kt
+++ src/main/kotlin/com/johnlindquist/acejump/ui/JumpInfo.kt
@@ -2,6 +2,7 @@ package com.johnlindquist.acejump.ui
 
 import com.intellij.openapi.editor.impl.EditorImpl
 import com.johnlindquist.acejump.search.*
+import com.johnlindquist.acejump.ui.JumpInfo.Alignment.*
 import java.awt.*
 import java.awt.AlphaComposite.*
 import java.awt.Color.*
@@ -18,6 +19,9 @@ class JumpInfo(private val tag: String, var query: String, val index: Int,
   var srcPoint = getPointFromVisualPosition(editor, originOffset).originalPoint
   var text = renderTag()
 
+  private var alignment: Alignment = ALIGN_RIGHT
+  enum class Alignment { ALIGN_TOP, ALIGN_BOTTOM, ALIGN_LEFT, ALIGN_RIGHT }
+
   fun renderTag(): String {
     var i = 0
     while (i + 1 < query.length &&
@@ -33,15 +37,13 @@ class JumpInfo(private val tag: String, var query: String, val index: Int,
   fun paintMe(g2d: Graphics2D, ac: AceCanvas) {
     tagPoint.translate(0, -ac.fbm.hOffset.toInt())
     g2d.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON)
-    g2d.composite = getInstance(SRC_OVER, 0.35.toFloat())
 
     val (tagX, tagY) = alignTag(ac)
     ac.registerTag(Pair(tagX, tagY), tag)
-    if (query.isNotEmpty())
-      highlight(ac, g2d, tagX, tagY)
+    highlight(ac, g2d, tagX, tagY)
 
     //just a touch of alpha
-    g2d.composite = getInstance(SRC_OVER, 0.85.toFloat())
+    g2d.composite = getInstance(SRC_OVER, 1.toFloat())
 
     //the foreground text
     g2d.font = ac.fbm.font
@@ -50,54 +52,83 @@ class JumpInfo(private val tag: String, var query: String, val index: Int,
   }
 
   private fun alignTag(ac: AceCanvas): Pair<Int, Int> {
-    val y = tagPoint.y //- ac.fbm.rectHOffset.toInt()
+    val y = tagPoint.y - ac.fbm.rectHOffset.toInt()
     val x = tagPoint.x + ac.fbm.fontWidth
     val lineOffset = getLengthFromStartToOffset(editor, index + query.length)
     val startOfNextLine = getLeadingCharacterOffset(editor, line + 1)
     val startOfThisLine = getLineStartOffset(editor, line)
     val startOfPrevLine = getLeadingCharacterOffset(editor, line - 1)
-    val pLineOffset = getLengthFromStartToOffset(editor, startOfPrevLine)
-    val nLineOffset = getLengthFromStartToOffset(editor, startOfNextLine)
+    val previousLineOffset = getLengthFromStartToOffset(editor, startOfPrevLine)
+    val nextLineOffset = getLengthFromStartToOffset(editor, startOfNextLine)
     val nextLineLength = getNextLineLength(editor, index)
     val previousLineLength = getPreviousLineLength(editor, index)
+    val topLine = getVisualLineAtTopOfScreen(editor)
+    val bottomLine = topLine + getScreenHeight(editor)
+    val previousCharIndex = Math.max(0, index - 1)
+    val alignTop = Pair(x - ac.fbm.fontWidth, y - ac.fbm.lineHeight)
+    val alignBottom = Pair(x - ac.fbm.fontWidth, y + ac.fbm.lineHeight)
+    val alignLeft = Pair(srcPoint.x - ac.fbm.fontWidth * (text.length), y)
+    val alignRight = Pair(x, y)
 
-    val right = Pair(x, y)
-    if (query.isNotEmpty()) {
-      val previousCharIndex = Math.max(0, index - 1)
-      val previousChar = document[previousCharIndex]
-      val previousCharIsOnSameLine = startOfThisLine < previousCharIndex
-      val alignTop = Pair(x - ac.fbm.fontWidth, y - ac.fbm.lineHeight)
-      val alignBottom = Pair(x - ac.fbm.fontWidth, y + ac.fbm.lineHeight)
-      val alignLeft = Pair(srcPoint.x - ac.fbm.fontWidth * (text.length), y)
+    val canAlignTop =
+      ac.isFree(alignTop) &&
+        (topLine..bottomLine).contains(line - 1) &&
+        (previousLineLength < lineOffset || previousLineOffset > lineOffset)
 
-      if (ac.isFree(alignBottom) && (nextLineLength < lineOffset ||
-        nLineOffset > lineOffset)) {
+    val canAlignBottom =
+      ac.isFree(alignBottom) &&
+        (topLine..bottomLine).contains(line + 1) &&
+        (nextLineLength < lineOffset || nextLineOffset > lineOffset)
+
+    val canAlignLeft =
+      startOfThisLine < previousCharIndex && ac.isFree(alignLeft)
+
+    if (query.isNotEmpty())
+      if (canAlignBottom) {
+        alignment = ALIGN_BOTTOM
         return alignBottom
-      } else if (ac.isFree(alignTop) && (previousLineLength < lineOffset ||
-        pLineOffset > lineOffset)) {
+      } else if (canAlignTop) {
+        alignment = ALIGN_TOP
         return alignTop
-      } else if (previousCharIsOnSameLine && ac.isFree(alignLeft)) {
+      } else if (canAlignLeft) {
+        alignment = ALIGN_LEFT
         return alignLeft
       }
-    }
-    return right
+
+    return alignRight
   }
 
   private fun highlight(ac: AceCanvas, g2d: Graphics2D, x: Int, y: Int) {
+    if (query.isEmpty())
+      return
+
     var tagWidth = text.length * ac.fbm.fontWidth
     var searchWidth = query.length * ac.fbm.fontWidth
     var tagX = x
     val lastQueryChar = query.last()
     val correspondingChar = document[index + query.length - 1].toLowerCase()
-    g2d.color = green
-    if (lastQueryChar == tag.first() && lastQueryChar != correspondingChar) {
-      g2d.fillRect(tagX, y, ac.fbm.fontWidth, ac.fbm.lineHeight.toInt())
-      tagX += ac.fbm.fontWidth
-      tagWidth -= ac.fbm.fontWidth
-      searchWidth -= ac.fbm.fontWidth
+    g2d.composite = getInstance(SRC_OVER, 0.40.toFloat())
+
+    fun highlightAlreadyTyped() {
+      g2d.color = green
+      if (lastQueryChar == tag.first() && lastQueryChar != correspondingChar) {
+        g2d.fillRect(tagX, y, ac.fbm.fontWidth, ac.fbm.lineHeight.toInt())
+        tagX += ac.fbm.fontWidth
+        tagWidth -= ac.fbm.fontWidth
+        searchWidth -= ac.fbm.fontWidth
+      }
+      g2d.fillRect(srcPoint.x - 1, tagPoint.y, searchWidth, ac.fbm.lineHeight)
     }
-    g2d.fillRect(srcPoint.x - 1, tagPoint.y, searchWidth, ac.fbm.lineHeight + 1)
-    g2d.color = yellow
-    g2d.fillRect(tagX, y, tagWidth, ac.fbm.lineHeight)
+
+    fun highlightRemaining() {
+      g2d.color = yellow
+      if(alignment != ALIGN_RIGHT)
+        g2d.composite = getInstance(SRC_OVER, 1.toFloat())
+
+      g2d.fillRect(tagX, y, tagWidth, ac.fbm.lineHeight)
+    }
+
+    highlightAlreadyTyped()
+    highlightRemaining()
   }
 }
