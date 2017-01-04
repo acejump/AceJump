096bae46f1b90dfbfd7202fb8f8901bb5f2408f1
Breandan Considine <breandan.considine@gmail.com>
2017-01-04 02:10:15 -0500

    migrate build.gradle to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.intellij.IntelliJPluginExtension
+
+buildscript {
+  repositories {
+    gradleScriptKotlin()
+    maven { setUrl("http://dl.bintray.com/jetbrains/intellij-plugin-service") }
+  }
+
+  dependencies {
+    classpath(kotlinModule("gradle-plugin"))
+  }
+}
+
+plugins {
+  id("org.jetbrains.intellij") version "0.1.10"
+}
+
+apply {
+  plugin("org.jetbrains.intellij")
+  plugin("kotlin")
+}
+
+configure<IntelliJPluginExtension> {
+  version = "171.2014.21"
+  pluginName = "AceJump"
+  updateSinceUntilBuild = false
+}
+
+group = "com.johnlindquist"
+version = "3.1.6"
