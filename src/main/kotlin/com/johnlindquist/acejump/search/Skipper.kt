bebc27f9eff35e2949a1b586e2bb814d084904ee
breandan <breandan.considine@gmail.com>
2017-06-16 20:56:51 -0400

    use scrollingModel for skip

--- src/main/kotlin/com/johnlindquist/acejump/search/Skipper.kt
+++ src/main/kotlin/com/johnlindquist/acejump/search/Skipper.kt
@@ -1,7 +1,16 @@
 package com.johnlindquist.acejump.search
 
+import com.intellij.openapi.editor.LogicalPosition
+import com.intellij.openapi.editor.ScrollType
+import com.johnlindquist.acejump.ui.AceUI.editor
+
 object Skipper {
-  fun ifQueryExistsSkipToNextOccurenceInEditor(query: String) {}
+  fun ifQueryExistsSkipToNextOccurenceInEditor(query: String) {
+    editor.scrollingModel.scrollTo(findPosition(query), ScrollType.CENTER)
+  }
+
+
+  private fun findPosition(query: String): LogicalPosition = editor.offsetToLogicalPosition(0)
+
   fun maximizeCoverageOfNextOccurence() {}
 }
-
