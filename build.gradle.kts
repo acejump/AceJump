a7fccc0c39f024d101d0450145bfd40cb7a19810
breandan <breandan.considine@gmail.com>
2017-08-22 17:19:21 -0400

    Kotlin 1.1 now defaults to JVM 1.6 bytecode, we need JVM 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions.jvmTarget = "1.8"
+}
+
 plugins {
   id("org.jetbrains.intellij") version "0.2.15"
   id("org.jetbrains.kotlin.jvm") version "1.1.3"
