273311d16b7fce81ff5effc036efdd5cfabcb7ff
breandan <bre@ndan.co>
2018-10-13 21:52:58 -0400

    move project settings into build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,16 @@ tasks {
 }
 
 plugins {
-  kotlin("jvm") version "1.3.0-rc-116"
+  idea apply true
+  kotlin("jvm") version "1.3.0-rc-146"
   id("org.jetbrains.intellij") version "0.3.11"
+  id("org.jetbrains.gradle.plugin.idea-ext") version "0.4.2"
+}
+
+idea {
+  project {
+    // TODO
+  }
 }
 
 intellij {
