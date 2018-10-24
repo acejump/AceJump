4e1e8d0692577f34558b3eb687dac45a1b2ffedb
breandan <bre@ndan.co>
2018-10-24 03:23:01 -0400

    store all settings in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+import org.gradle.internal.impldep.org.testng.TestNG
+import org.jetbrains.gradle.ext.GradleTask
+import org.jetbrains.gradle.ext.ProjectSettings
+import org.jetbrains.gradle.ext.Remote
+import org.jetbrains.gradle.ext.RunConfiguration
 import org.jetbrains.intellij.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -15,14 +20,24 @@ tasks {
 
 plugins {
   idea apply true
-  kotlin("jvm") version "1.3.0-rc-116"
+  kotlin("jvm") version "1.3.0-rc-190"
   id("org.jetbrains.intellij") version "0.3.11"
-  id("org.jetbrains.gradle.plugin.idea-ext") version "0.4.2"
+  id("org.jetbrains.gradle.plugin.idea-ext") version "0.3" apply true
 }
 
 idea {
   project {
-    // TODO
+    (this as ExtensionAware)
+    configure<ProjectSettings> {
+      (this as ExtensionAware)
+      runConfigurations {
+        create("runIde", org.jetbrains.gradle.ext.Application::class.java) {
+          beforeRun.register("runIdx", GradleTask::class.java) {
+            task = task("runIde")
+          }
+        }
+      }
+    }
   }
 }
 
