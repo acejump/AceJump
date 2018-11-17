11d6473d8d0ab704be9ed243df77642057b70a9e
breandan <bre@ndan.co>
2018-11-17 13:42:56 -0500

    some functional tests for #139 and performance tests for #207 #217

--- src/test/kotlin/AceTest.kt
+++ src/test/kotlin/AceTest.kt
@@ -3,59 +3,74 @@ import com.intellij.openapi.fileTypes.PlainTextFileType
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
 import com.intellij.util.ui.UIUtil
 import org.acejump.control.AceAction
-import org.acejump.search.Finder
 import org.acejump.view.Canvas
+import kotlin.system.measureTimeMillis
 
 /**
+ * Functional test cases and end-to-end performance tests.
+ *
  * @see com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
  */
+
 class AceTest : LightCodeInsightFixtureTestCase() {
+  fun `test that scanner finds all occurrences of single character`() =
+    assertEquals("test test test".lookFor("t"), setOf(0, 3, 5, 8, 10, 13))
+
+  fun `test empty results for an absent query`() =
+    assertEmpty("test test test".lookFor("best"))
+
+  fun `test sticky results for a query with extra characters`() =
+    assertEquals("test test test".lookFor("testz"), setOf(0, 5, 10))
+
+  fun `test a query inside text with some variations`() =
+    assertEquals("abcd dabc cdab".lookFor("cd"), setOf(2, 10))
+
+  fun `test a query containing a space character`() =
+    assertEquals("abcd dabc cdab".lookFor("cd "), setOf(2))
+
+  fun `test a query containing a { character`() =
+    assertEquals("abcd{dabc cdab".lookFor("cd{"), setOf(2))
+
+  fun `test that jumping to first occurrence succeeds`() {
+    val results = "<caret>testing 1234".lookFor("1")
+
+    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_ENTER)
+    // TODO: Verify that the caret has moved to "testing <caret>1234"
+  }
+
+  // Enforces the results are available in less than 100ms
+  private fun String.lookFor(query: String) =
+    myFixture.run {
+      maybeWarmup(this@lookFor, query)
+      assert(measureTimeMillis { this@lookFor.justDoQuery(query) } < 100)
+      editor.markupModel.allHighlighters.map { it.startOffset }.toSet()
+    }
+
+  var warmup = true
+  // Should run exactly once to warm up the JVM
+  private fun maybeWarmup(text: String, query: String) {
+    if (warmup) {
+      text.justDoQuery(query)
+      myFixture.performEditorAction(IdeActions.ACTION_EDITOR_ESCAPE)
+      UIUtil.dispatchAllInvocationEvents()
+      warmup = false
+    }
+  }
+
+  // Just does a query without enforcing any time limit
+  private fun String.justDoQuery(query: String) {
+    myFixture.run {
+      configureByText(PlainTextFileType.INSTANCE, this@justDoQuery)
+      testAction(AceAction())
+      type(query)
+      UIUtil.dispatchAllInvocationEvents()
+    }
+  }
+
   override fun tearDown() {
     myFixture.performEditorAction(IdeActions.ACTION_EDITOR_ESCAPE)
     UIUtil.dispatchAllInvocationEvents()
     assert(Canvas.jumpLocations.isEmpty())
     super.tearDown()
   }
-
-  fun `test finder finds all occurrences`() {
-    val code = "test test test"
-
-    invokeAndSearch(code, "test")
-
-    assert(Canvas.jumpLocations.size == 3)
-  }
-
-  fun `test finder finds nothing`() {
-    val code = "test test test"
-
-    invokeAndSearch(code, "qest")
-
-    assert(Canvas.jumpLocations.isEmpty())
-  }
-
-
-  fun `test finder is sticky`() {
-    val code = "test test test"
-
-    invokeAndSearch(code, "tezt")
-
-    assert(Canvas.jumpLocations.size == 3)
-  }
-
-  fun `test jump to first occurrence`() {
-    val code = "<caret>testing 1234"
-
-    invokeAndSearch(code, "1")
-
-    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_ENTER)
-    // TODO: Verify that the caret has moved to "testing <caret>1234"
-  }
-
-  private fun invokeAndSearch(code: String, query: String) =
-    myFixture.run {
-      configureByText(PlainTextFileType.INSTANCE, code)
-      testAction(AceAction())
-      type(query)
-      UIUtil.dispatchAllInvocationEvents()
-    }
 }
