8e70719dd797e2c05d39786f62b95647b6743daa
breandan <bre@ndan.co>
2019-04-12 01:32:48 -0400

    bump kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ tasks {
 
 plugins {
   idea apply true
-  kotlin("jvm") version "1.3.21"
+  kotlin("jvm") version "1.3.30"
   id("org.jetbrains.intellij") version "0.4.7"
   id("org.jetbrains.gradle.plugin.idea-ext") version "0.3" apply true
 }
