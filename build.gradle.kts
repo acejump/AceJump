1f4841792e0eeb9b7b0bc29895eb6d3825a5e52b
breandan.considine <breandan.considine@gmail.com>
2019-07-15 00:14:38 -0400

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ tasks {
 
 plugins {
   idea apply true
-  kotlin("jvm") version "1.3.21"
-  id("org.jetbrains.intellij") version "0.4.5"
+  kotlin("jvm") version "1.3.41"
+  id("org.jetbrains.intellij") version "0.4.9"
   id("org.jetbrains.gradle.plugin.idea-ext") version "0.3" apply true
 }
 
