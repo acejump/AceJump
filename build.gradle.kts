0c574069ff23433658354bc04c14383e7bdbab90
breandan.considine <breandan.considine@gmail.com>
2019-08-08 23:45:41 -0400

    roll back version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ tasks {
 plugins {
   idea apply true
   kotlin("jvm") version "1.3.41"
-  id("org.jetbrains.intellij") version "0.4.10"
+  id("org.jetbrains.intellij") version "0.4.9"
   id("org.jetbrains.gradle.plugin.idea-ext") version "0.3" apply true
 }
 
