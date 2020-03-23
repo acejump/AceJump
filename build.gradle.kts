70fd2c465af1667b27ac219fdb0ebd5aad015cb0
breandan <bre@ndan.co>
2020-03-23 12:51:42 -0400

    bump intellij gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 plugins {
   idea apply true
   kotlin("jvm") version "1.3.70"
-  id("org.jetbrains.intellij") version "0.4.16"
+  id("org.jetbrains.intellij") version "0.4.17"
   id("de.fayard.refreshVersions") version "0.8.6"
 }
 
