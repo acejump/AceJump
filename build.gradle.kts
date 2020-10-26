cb6a83794f4b59acb5ea77b0a2981daec412f17f
breandan <bre@ndan.co>
2020-10-25 23:20:49 -0400

    update changelog plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
   idea apply true
   kotlin("jvm") version "1.3.72"
   id("org.jetbrains.intellij") version "0.5.0"
-  id("org.jetbrains.changelog") version "0.4.0"
+  id("org.jetbrains.changelog") version "0.6.2"
 }
 
 tasks {
