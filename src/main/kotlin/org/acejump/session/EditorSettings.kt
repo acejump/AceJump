eb1bbb2e03b781a1a61926116c1859e61b381992
chylex <contact@chylex.com>
2020-12-13 08:04:15 +0100

    Prevent editing document while AceJump is active

--- src/main/kotlin/org/acejump/session/EditorSettings.kt
+++ src/main/kotlin/org/acejump/session/EditorSettings.kt
@@ -6,24 +6,32 @@ import com.intellij.openapi.editor.Editor
  * Holds [Editor] caret settings. The settings are saved the moment a [Session] starts, modified to indicate AceJump states, and restored
  * once the [Session] ends.
  */
-internal data class EditorSettings(private val isBlockCursor: Boolean, private val isBlinkCaret: Boolean) {
+internal data class EditorSettings(private val isBlockCursor: Boolean, private val isBlinkCaret: Boolean, private val isReadOnly: Boolean) {
   companion object {
     fun setup(editor: Editor): EditorSettings {
       val settings = editor.settings
+      val document = editor.document
+      
       val original = EditorSettings(
         isBlockCursor = settings.isBlockCursor,
-        isBlinkCaret = settings.isBlinkCaret
+        isBlinkCaret = settings.isBlinkCaret,
+        isReadOnly = !document.isWritable
       )
       
       settings.isBlockCursor = true
       settings.isBlinkCaret = false
+      document.setReadOnly(true)
       
       return original
     }
   }
   
-  fun restore(editor: Editor) = editor.settings.let {
-    it.isBlockCursor = isBlockCursor
-    it.isBlinkCaret = isBlinkCaret
+  fun restore(editor: Editor) {
+    val settings = editor.settings
+    val document = editor.document
+    
+    settings.isBlockCursor = isBlockCursor
+    settings.isBlinkCaret = isBlinkCaret
+    document.setReadOnly(isReadOnly)
   }
 }
