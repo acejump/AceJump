41071dbaf9028362f5f85f0c467da2502403ddcd
chylex <contact@chylex.com>
2021-11-14 15:48:58 +0100

    Workaround tag misalignment on high DPI screens
    Fixes #362.

--- src/main/kotlin/org/acejump/view/TextHighlighter.kt
+++ src/main/kotlin/org/acejump/view/TextHighlighter.kt
@@ -164,7 +164,7 @@ internal class TextHighlighter {
       val end = EditorOffsetCache.Uncached.offsetToXY(editor, endOffset)
 
       g.color = AceConfig.textHighlightColor
-      g.fillRect(start.x, start.y + 1, end.x - start.x, editor.lineHeight - 1)
+      g.fillRect(start.x, start.y, end.x - start.x, editor.lineHeight)
 
       g.color = AceConfig.tagBackgroundColor
       g.drawRect(start.x, start.y, end.x - start.x, editor.lineHeight)
@@ -213,7 +213,7 @@ internal class TextHighlighter {
       val lastCharWidth = editor.component.getFontMetrics(font).charWidth(char)
 
       g.color = AceConfig.textHighlightColor
-      g.fillRect(pos.x, pos.y + 1, lastCharWidth, editor.lineHeight - 1)
+      g.fillRect(pos.x, pos.y, lastCharWidth, editor.lineHeight)
 
       g.color = AceConfig.tagBackgroundColor
       g.drawRect(pos.x, pos.y, lastCharWidth, editor.lineHeight)
