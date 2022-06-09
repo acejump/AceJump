5271d97c00461007cf8c52bf63a69ce3ca2245fb
breandan <bre@ndan.co>
2022-06-09 00:55:13 -0700

    new kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   idea apply true
-  kotlin("jvm") version "1.7.0-RC"
+  kotlin("jvm") version "1.7.0"
   id("org.jetbrains.intellij") version "1.6.0"
   id("org.jetbrains.changelog") version "1.3.1"
   id("com.github.ben-manes.versions") version "0.42.0"
